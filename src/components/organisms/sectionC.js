7c0645fa20b370097688f16e61c9f2aded0898a8
iltommi1995 <tomasdanielavilavisintin@yahoo.it>
2020-04-04 13:32:09 +0200

    haha

--- src/components/organisms/sectionC.js
+++ src/components/organisms/sectionC.js
@@ -1,14 +1,16 @@
 import React from "react";
+import H2 from "../atoms/h2";
+import P from "../atoms/p";
 
 export default function SectionC() {
   return (
     <section id="section-c" class="grid">
       <div class="content-wrap">
-        <h2 class="content-title">We handle all of your digital needs</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente hic
-          et repellat dolorem non illo repudiandae sint officiis quod error.
-        </p>
+        <H2 text="We handle all of your digital needs" />
+        <P
+          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente hic
+          et repellat dolorem non illo repudiandae sint officiis quod error."
+        />
       </div>
     </section>
   );
