8f764a001ed8a73b2af005094f4a882d582c3fd2
iltommi1995 <tomasdanielavilavisintin@yahoo.it>
2020-04-04 14:00:54 +0200

    ultima

--- src/components/organisms/sectionD.js
+++ src/components/organisms/sectionD.js
@@ -1,25 +1,27 @@
 import React from "react";
+import H2 from "../atoms/h2";
+import P from "../atoms/p";
 
 export default function SectionD() {
   return (
     <section id="section-d" class="grid">
       <div class="box">
-        <h2 class="content-title">Contact Us</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis esse
+        <H2 text="Contact Us" />
+        <P
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis esse
           mollitia facilis veniam perspiciatis veritatis accusamus labore
-          consequatur quidem explicabo.
-        </p>
-        <p>contact@gmail.com</p>
+          consequatur quidem explicabo."
+        />
+        <P text="contact@gmail.com" />
       </div>
       <div class="box">
-        <h2 class="content-title">About Our Company</h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+        <H2 text="About Our Company" />
+        <P
+          text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
           obcaecati sint modi similique quas sequi adipisci quibusdam inventore
           dolor voluptatibus, laborum omnis quo delectus vitae ducimus veniam
-          impedit. Illum, nostrum?
-        </p>
+          impedit. Illum, nostrum?"
+        />
       </div>
     </section>
   );
